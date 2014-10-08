eac923a43f957adc592554b4ef7258d9878c538b
AutonomousHC <hanfordpatrick@gmail.com>
2014-10-08 14:55:14 -0400

    Organizational Edits
    Made some minor edits to make the code easier on the eyes.

--- functions.php
+++ functions.php
@@ -14,38 +14,32 @@ include 'encryption.php';
 $validpw = false;
 
 
-if(!isset($_POST['password']) || $_POST['password'] == '')
-{
-$mail = '<div style="color:red">You will be asked for an encryption key provided to you by Autonomous. Without this, the impersonation will fail.</div>';
-$validpw = false;
-} elseif($_POST['password'] != PASSWORD)
-{
-$mail = '<div style="color:red">Invalid encryption key. You do not belong here.</div>';
-$validpw = false;
+if (!isset($_POST['password']) || $_POST['password'] == '') {
+    $mail    = '<div style="color:red">You will be asked for an encryption key provided to you by Autonomous. Without this, the impersonation will fail.</div>';
+    $validpw = false;
+} elseif ($_POST['password'] != PASSWORD) {
+    $mail    = '<div style="color:red">Invalid encryption key. You do not belong here.</div>';
+    $validpw = false;
 } else {
-$validpw = true;
+    $validpw = true;
 }
 
 
-if(isset($_POST['to']) && isset($_POST['from']) && isset($_POST['fromname']) && isset($_POST['replyto']) && isset($_POST['subject']) && isset($_POST['message']) && $validpw)
-{
-$headers = 'From: '.$_POST['fromname'].' <'.$_POST['from'].'>' . "\r\n" .
-'Reply-To: '. $_POST['replyto'] . "\r\n";
-
-$mail = mail($_POST['to'],$_POST['subject'],$_POST['message'],$headers);
-
-if($mail)
-{
-$mail = '<div style="color:green">Impersonation Successful.</div>';
+if (isset($_POST['to']) && isset($_POST['from']) && isset($_POST['fromname']) && isset($_POST['replyto']) && isset($_POST['subject']) && isset($_POST['message']) && $validpw) {
+    $headers = 'From: ' . $_POST['fromname'] . ' <' . $_POST['from'] . '>' . "\r\n" . 'Reply-To: ' . $_POST['replyto'] . "\r\n";
+    
+    $mail = mail($_POST['to'], $_POST['subject'], $_POST['message'], $headers);
+    
+    if ($mail) {
+        $mail = '<div style="color:green">Impersonation Successful.</div>';
+    } else {
+        $mail = '<div style="color:red">Access Denied.</div>';
+    }
+    //Notifies end-user that a required field hadn't been completed.
 } else {
-$mail = '<div style="color:red">Access Denied.</div>';
-}
-//Notifies end-user that a required field hadn't been completed.
-} else {
-if(!isset($mail))
-{
-$mail = '<div style="color:red">Leave no stone unturned.</div>';
-}
+    if (!isset($mail)) {
+        $mail = '<div style="color:red">Leave no stone unturned.</div>';
+    }
 }
 
 
